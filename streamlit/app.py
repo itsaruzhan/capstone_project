9a7508a0935b14bbde9180e9ee341d3750d92911
itsaruzhan <anarbayeva.aru@gmail.com>
2023-05-21 21:20:02 +0600

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -11,7 +11,7 @@ if 'data' not in st.session_state:
 st.write("""
     # Загружай новости NUR.KZ и узнай категорию!
     """)
-filename = "./nlp-model-ru.joblib"
+filename = "streamlit/nlp-model-ru.joblib"
 
 model = joblib.load(filename)
 
